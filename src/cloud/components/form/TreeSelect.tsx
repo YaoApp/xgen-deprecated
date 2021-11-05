998f2e58db1a4daa7288955dcacf3db963f6835e
MatrixAge <xiewendao123@foxmail.com>
2021-11-05 19:04:04 +0800

    [add] 新增TreeSelect

---
+++ src/cloud/components/form/TreeSelect.tsx
@@ -0,0 +1,61 @@
+import { TreeSelect } from 'antd'
+import { useMemo, useState } from 'react'
+import { request } from 'umi'
+
+import { Item } from '@/components'
+
+import type { TreeSelectProps } from 'antd'
+
+interface IProps extends TreeSelectProps<any> {
+	name: string
+	bind?: string
+	label?: string
+	string?: '1' | '0' | undefined
+	rules: Array<any>
+	remote: {
+		api: string
+		query: {
+			keyword: string
+			select: Array<string>
+		}
+	}
+	options: Array<any>
+}
+
+const Index = (props: IProps) => {
+	const [data, setData] = useState<TreeSelectProps<any>['treeData']>([])
+
+	const getData = async () => {
+		const data = await request(
+			`${props.remote.api}?select=${props.remote.query.select.join(',')}`
+		)
+
+		setData(data)
+	}
+
+	const real_props = useMemo(() => {
+		const _props = { ...props }
+
+		if (_props.remote) {
+			getData()
+		}
+
+		if (_props.options) {
+			setData(_props.options)
+		}
+
+		return _props
+	}, [props])
+
+	return (
+		<Item {...(props as any)}>
+			<TreeSelect
+				{...real_props}
+				treeData={data}
+				placeholder={props.placeholder || `请选择${props.label}`}
+			></TreeSelect>
+		</Item>
+	)
+}
+
+export default window.$app.memo(Index)
