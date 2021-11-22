eb8de874f9eee0cb0c67c155f6415db649481687
MatrixAge <xiewendao123@foxmail.com>
2021-11-22 18:48:49 +0800

    [add] 针对iframe显示所有content布局

--- src/pages/iframe/index.tsx
+++ src/pages/iframe/index.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react'
 import { history } from 'umi'
 
 const Index = () => {
 	const { location } = history
+
+	useEffect(() => {
+		document.body.style.overflow = 'hidden'
+
+		return () => {
+			document.body.style.overflow = 'auto'
+		}
+	}, [])
 
 	return (
 		<iframe
