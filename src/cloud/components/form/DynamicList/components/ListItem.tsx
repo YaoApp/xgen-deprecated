4e4abfb79bc4da1ae7d0378a0a8421ec89bf5557
MatrixAge <xiewendao123@foxmail.com>
2022-01-13 17:27:18 +0800

    [add] DynamicList支持label

--- src/cloud/components/form/DynamicList/components/ListItem.tsx
+++ src/cloud/components/form/DynamicList/components/ListItem.tsx
@@ -17,7 +17,7 @@ interface IPropsItem {
 const Index = (props: IPropsItem) => {
 	const { item, it, item_key, col_key, onChange } = props
 	const { value, ...props_no_value } = it.edit.props
-	const text = useItemText(it, item)
+	const text = it.edit.type === 'label' ? it.edit.props.value : useItemText(it, item)
 
 	const change = (v: any) => {
 		onChange(item_key, v)
@@ -25,50 +25,56 @@ const Index = (props: IPropsItem) => {
 
 	return (
 		<Col span={it.width}>
-			<Popover
-				id='dynamic_list_td_popover'
-				overlayClassName='td_popover dynamic_list'
-				placement='topLeft'
-				trigger='click'
-				destroyTooltipOnHide={{ keepParent: false }}
-				content={
-					<Form
-						className='w_100 flex'
-						name={`quick_table_td_${item_key}_${col_key}`}
-						initialValues={{
-							[it.key]: item[it.key]
-						}}
-						onFinish={(v) => change(v)}
-					>
-						<Dynamic
-							type='form'
-							name={it.edit.type}
-							props={{
-								...props_no_value,
-								name: it.key,
-								label: it.title,
-								value: item[it.key],
-								style: { width: 240 }
-							}}
-						></Dynamic>
-						<Button
-							className='ml_12'
-							type='primary'
-							htmlType='submit'
-							icon={<CheckOutlined></CheckOutlined>}
-						></Button>
-					</Form>
-				}
-			>
-				<div
-					className={clsx([
-						'td_text w_100 border_box h_100 flex align_center',
-						!item[it.key] && 'empty'
-					])}
-				>
+			{it.edit.type === 'label' ? (
+				<div className='td_text label w_100 border_box h_100 flex justify_center align_center'>
 					<span className='text'>{text}</span>
 				</div>
-			</Popover>
+			) : (
+				<Popover
+					id='dynamic_list_td_popover'
+					overlayClassName='td_popover dynamic_list'
+					placement='topLeft'
+					trigger='click'
+					destroyTooltipOnHide={{ keepParent: false }}
+					content={
+						<Form
+							className='w_100 flex'
+							name={`quick_table_td_${item_key}_${col_key}`}
+							initialValues={{
+								[it.key]: item[it.key]
+							}}
+							onFinish={(v) => change(v)}
+						>
+							<Dynamic
+								type='form'
+								name={it.edit.type}
+								props={{
+									...props_no_value,
+									name: it.key,
+									label: it.title,
+									value: item[it.key],
+									style: { width: 240 }
+								}}
+							></Dynamic>
+							<Button
+								className='ml_12'
+								type='primary'
+								htmlType='submit'
+								icon={<CheckOutlined></CheckOutlined>}
+							></Button>
+						</Form>
+					}
+				>
+					<div
+						className={clsx([
+							'td_text w_100 border_box h_100 flex align_center',
+							!item[it.key] && 'empty'
+						])}
+					>
+						<span className='text'>{text}</span>
+					</div>
+				</Popover>
+			)}
 		</Col>
 	)
 }
